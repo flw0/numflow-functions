341a0e687150541fcb91de86a9611c333d13c939
Douwe den Blanken <douwedb@gmail.com>
2020-04-18 11:34:47 +0200

    Made rollup config work with regular TS, so no React (TSX)

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import typescript from "rollup-plugin-typescript2";
 import commonjs from "rollup-plugin-commonjs";
 import external from "rollup-plugin-peer-deps-external";
+import filesize from 'rollup-plugin-filesize';
 import scss from "rollup-plugin-scss";
 
 import pkg from "./package.json";
@@ -25,11 +26,15 @@ export default {
   plugins: [
     external(),
     scss(),
+    filesize(),
     typescript({
       rollupCommonJSResolveHack: true,
       exclude: "**/__tests__/**",
       clean: true,
     }),
+    // babel({
+    //   exclude: "node_modules/**",
+    // }),
     commonjs({
       include: ["node_modules/**"],
       namedExports: {
