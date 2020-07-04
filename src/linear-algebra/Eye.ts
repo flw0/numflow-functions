0e6b66d74ac2ccff0e6952f17f303da04b70c612
Douwe den Blanken <douwedb@gmail.com>
2020-07-05 01:43:17 +0200

    Added example of Eye matrix representation

--- src/linear-algebra/Eye.ts
+++ src/linear-algebra/Eye.ts
@@ -5,6 +5,19 @@ function eye(size: number) {
 }
 
 class Eye extends Matrix {
+  /**
+   * Create a new identity matrix of a given size
+   *
+   * When running `new Eye(3)`, the result looks like this:
+   * ```
+   * ┌           ┐
+   * │  1  0  0  │
+   * │  0  1  0  │
+   * │  0  0  1  │
+   * └           ┘
+   * ```
+   * @param size The size of the identity matrix to be created
+   */
   constructor(size: number) {
     const result = new Array(size * size);
 
