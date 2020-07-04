e7c9bb74392e072b44870225c529ee1606b9c14c
Douwe den Blanken <douwedb@gmail.com>
2020-07-05 01:43:42 +0200

    Added radians and degrees conversions

---
+++ src/trigonometry/deg2rad.ts
@@ -0,0 +1,7 @@
+import pi from "./pi";
+
+function deg2rad(a: number) {
+  return a * (pi / 180);
+}
+
+export default deg2rad;
---
+++ src/trigonometry/rad2deg.ts
@@ -0,0 +1,7 @@
+import pi from "./pi";
+
+function rad2deg(a: number) {
+  return a * (180 / pi);
+}
+
+export default rad2deg;
